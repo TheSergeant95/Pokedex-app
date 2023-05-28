e12fd64cd2611ec223f6412dbaf19e6eefebc5f1
DESKTOP-NLM5CTP\unlea <sergeant.krut@gmail.com>
2023-05-28 03:51:48 +0300

    asyncAction refactoring

--- src/store/pokemonList/asyncActions.ts
+++ src/store/pokemonList/asyncActions.ts
@@ -1,12 +1,8 @@
 import { ThunkResult, AppDispatch } from '..';
-
 import { getPokemonByName } from '../../api';
-
 import { filterPost, searchPost } from '../../utils';
-
 import { Pokemon } from '../pokemon/types';
 import { PokemonCriteria } from '../pokemonConsts/types';
-
 import {
 	fetchPokemonsRequest,
 	setCurrentCount,
@@ -24,15 +20,16 @@ export const fetchPokemons =
 	): ThunkResult<void> =>
 	async (dispatch: AppDispatch) => {
 		dispatch(fetchPokemonsRequest());
+
 		try {
-			const offset = page * limit - limit;
+			const offset = (page - 1) * limit;
 			const filteredPokemons = filterPost(searchPost(pokemons, searchQuery), selectedTypes);
-			const paginatedPokemons = filteredPokemons.filter(
-				(pokemon, index) => index >= offset && index < offset + limit,
-			);
+			const paginatedPokemons = filteredPokemons.slice(offset, offset + limit);
+
 			const pokemonList: Pokemon[] = await Promise.all(
 				paginatedPokemons.map(async (pokemon: PokemonCriteria) => {
 					const pokemonDetails = await getPokemonByName(pokemon.name);
+
 					return {
 						id: pokemonDetails.id,
 						name: pokemonDetails.name,
@@ -44,6 +41,7 @@ export const fetchPokemons =
 					};
 				}),
 			);
+
 			dispatch(setCurrentCount(filteredPokemons.length));
 			dispatch(fetchPokemonsSuccess(pokemonList));
 		} catch (error) {
